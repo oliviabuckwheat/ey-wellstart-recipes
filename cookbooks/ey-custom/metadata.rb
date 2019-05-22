73cf3b91718764ff6a90bc4cfecfab9ee5b0cd0c
Manuel Ortega <manuel@lasindias.coop>
2019-05-22 10:50:03 -0700

    Added recipes we need for wellstart

--- cookbooks/ey-custom/metadata.rb
+++ cookbooks/ey-custom/metadata.rb
@@ -1,1 +1,4 @@
 name 'ey-custom'
+depends 'custom-redis'
+depends 'custom-sidekiq'
+depends 'custom-elasticsearch'
---
+++ cookbooks/ey-custom/recipes/after-main.rb
@@ -0,0 +1,3 @@
+include_recipe 'custom-redis'
+include_recipe 'custom-sidekiq'
+include_recipe 'custom-elasticsearch'
