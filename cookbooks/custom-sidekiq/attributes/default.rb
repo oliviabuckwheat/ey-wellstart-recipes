1180735846a50ee9d53f250e58bd9b9b317abf87
Manuel Ortega <manuel@lasindias.coop>
2019-06-22 05:01:17 -0700

    Added sidekiq settings

--- cookbooks/custom-sidekiq/attributes/default.rb
+++ cookbooks/custom-sidekiq/attributes/default.rb
@@ -11,4 +11,37 @@ default['sidekiq']['is_sidekiq_instance'] = (node['dna']['instance_role'] == 'ut
 #default['sidekiq']['is_sidekiq_instance'] = (node['dna']['instance_role'] == 'solo')
 
 # Default memory limit
-# default['sidekiq']['worker_memory'] = 400
+
+
+# Number of workers (not threads)
+default['sidekiq']['workers'] = 1
+
+# Concurrency
+default['sidekiq']['concurrency'] = 25
+
+# Queues
+default['sidekiq']['queues'] = {
+  # :queue_name => priority
+  :critical => 3,
+  :default => 2,
+  :mailers => 2,
+  :low => 1,
+}
+
+# Memory limit
+default['sidekiq']['worker_memory'] = 400 # MB
+
+# Verbose
+default['sidekiq']['verbose'] = false
+
+# Setting this to true installs a cron job that
+# regularly terminates sidekiq workers that aren't being monitored by monit,
+# and terminates those workers
+#
+# default: false
+default['sidekiq']['orphan_monitor_enabled'] = false
+
+# sidekiq_orphan_monitor cron schedule
+#
+# default: every 5 minutes
+default['sidekiq']['orphan_monitor_cron_schedule'] = "*/5 * * * *"
